d8ff1975dee2703306d1add149e656cf24e4ecbc
Radomir <radomir21182002@gmail.com>
2026-03-21 13:03:18 +0000

    some tests

--- project/train_test/tests.py
+++ project/train_test/tests.py
@@ -10,10 +10,12 @@ def main():
     ct_full = adapter.encrypt_block(pt, key)
     ct_r1 = adapter.encrypt_rounds(pt, key, 1)
     ct_r2 = adapter.encrypt_rounds(pt, key, 2)
+    ct_r10 = adapter.encrypt_rounds(pt, key, 10)
 
     print("full:", ct_full.hex())
     print("r1  :", ct_r1.hex())
     print("r2  :", ct_r2.hex())
+    print("r10 :", ct_r10.hex())
 
 
 if __name__ == "__main__":
